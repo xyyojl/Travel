9caf24fff39aed2a73d7f2d691c8783a68e48642
xyyojl <m13631897256@163.com>
2019-07-10 10:56:02 +0800

    进行思路的梳理

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,7 +20,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    @import '~styles/varibles.styl'
+    // @import '~styles/varibles.styl'
+    @import '~@/assets/styles/varibles.styl'
     .header
         display: flex
         height: .86rem
