bfd85319dc60b9cd48511fdd1023bddd09731252
xyyojl <m13631897256@163.com>
2019-07-14 16:42:55 +0800

    实现动态路由和banner布局、公用图片画廊组件拆分

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container" @click="handleGallaryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOption">
+                <swiper-slide v-for="(item, index) of imgs" :key="index">
+                    <img class="gallary-img" :src="item">
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        // 添加下面两个参数，解决轮播无法正常滚动
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    // 解决 .swiper-pagination 这个元素被隐藏
+    .wrapper >>> .swiper-container
+        overflow: inherit
+    .container
+        display: flex
+        flex-direction: column
+        justify-content: center
+        position: fixed
+        left: 0
+        right: 0
+        top: 0
+        bottom: 0
+        background: #000
+        .wrapper
+            // overflow: hidden
+            height: 0
+            width: 100%
+            padding-bottom: 100%
+            .gallary-img
+                width: 100%
+            .swiper-pagination
+                color: #fff
+                bottom: -1rem
+</style>
