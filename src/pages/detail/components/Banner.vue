08b4a659a78cf344b3df9d9013bf5b43dddd04b1
xyyojl <m13631897256@163.com>
2019-07-14 21:51:30 +0800

    <common-gallary             :imgs="gallaryImgs"             v-show="showGallary"             @close="handleGallaryClose"         ></common-gallary>

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -10,17 +10,19 @@
                 </div>
             </div>
         </div>
-        <common-gallary
-            :imgs="gallaryImgs"
-            v-show="showGallary"
-            @close="handleGallaryClose"
-        ></common-gallary>
+        <fade-animation>
+          <common-gallary
+              :imgs="gallaryImgs"
+              v-show="showGallary"
+              @close="handleGallaryClose"
+          ></common-gallary>
+        </fade-animation>
     </div>
-
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -34,7 +36,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleBannerClick () {
