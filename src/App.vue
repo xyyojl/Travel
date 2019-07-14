3e6c6a8d3c22b416f74debfcd3255e7531af56b9
xyyojl <m13631897256@163.com>
2019-07-14 21:30:19 +0800

    动态获取详情页面数据

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <keep-alive>
+    <keep-alive exclude="Detail">
+      <!-- 除了Detail之外都会被缓存 -->
       <router-view/>
     </keep-alive>
   </div>
--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        <detail-banner></detail-banner>
+        <detail-banner
+            :sightName="sightName"
+            :bannerImg="bannerImg"
+            :gallaryImgs="gallaryImgs"
+        ></detail-banner>
         <detail-header></detail-header>
         <div class="content">
-            <detail-list :list="list"></detail-list>
+            <detail-list :list="categoryList"></detail-list>
         </div>
     </div>
 </template>
@@ -12,6 +16,7 @@
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
@@ -21,28 +26,34 @@ export default {
   },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆门票',
-          children: [{
-            title: '成人三馆门票 - 优等'
-          }]
-        }]
-      }, {
-        title: '学生票',
-        children: [{
-          title: '成人五馆门票',
-          children: [{
-            title: '成人五馆门票 - 中等'
-          }]
-        }]
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      categoryList: []
     }
+  },
+  methods: {
+    getDetailInfo () {
+      //   axios.get('/api/detail.json?id=' + this.$route.params.id)
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.categoryList = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
